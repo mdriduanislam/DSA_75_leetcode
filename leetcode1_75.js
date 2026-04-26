3c9478a9dd4c1396a10e0afcf3edc40efea42668
mdriduanislam <mohhammadris786@gmail.com>
2026-04-26 23:52:12 +0600

    possible solutions of string reversing

--- leetcode1_75.js
+++ leetcode1_75.js
@@ -11,3 +11,36 @@ var mergeAlternately = function(word1, word2) {
     return mergedString;
 };
 console.log(mergeAlternately("abc","pqr"))//"
+
+
+let str = "hello";
+let reversed = str.split("").reverse().join("");
+
+function reverseString(str) {
+  let arr = str.split("");
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+
+  return arr.join("");
+}
+
+function reverseString(str) {
+  let result = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+
+  return result;
+}
+
+function reverseString(str) {
+  if (str === "") return "";
+  return reverseString(str.slice(1)) + str[0];
+}
