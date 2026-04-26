fcc1aa3d11c8b8fb0b84d6147c9bf0580b8031ae
mdriduanislam <mohhammadris786@gmail.com>
2026-04-27 00:26:09 +0600

    1 one 75 solved

--- leetcode1_75.js
+++ leetcode1_75.js
@@ -5,47 +5,12 @@ var mergeAlternately = function(word1, word2) {
     let w2 = word2.length;
     let max= Math.max(w1,w2)
     for (let i=0; i<max; i++){
+        if(i<w1)
         mergedString+=word1[i];
-        mergedString+=word2[w2-1-i];
+        if(i<w2)
+        mergedString+=word2[i];
     }
     return mergedString;
 };
-console.log(mergeAlternately("ab","pqr"))//"
+console.log(mergeAlternately("abc","pq"))
 
-// let str = "hello";
-// let reversed = str.split("").reverse().join("");
-// console.log(str)
-// console.log(reversed)
-
-
-// let str = "hello";
-// let reversed = str.split("").reverse().join("");
-
-// function reverseString(str) {
-//   let arr = str.split("");
-//   let left = 0;
-//   let right = arr.length - 1;
-
-//   while (left < right) {
-//     [arr[left], arr[right]] = [arr[right], arr[left]];
-//     left++;
-//     right--;
-//   }
-
-//   return arr.join("");
-// }
-
-// function reverseString(str) {
-//   let result = "";
-
-//   for (let i = str.length - 1; i >= 0; i--) {
-//     result += str[i];
-//   }
-
-//   return result;
-// }
-
-// function reverseString(str) {
-//   if (str === "") return "";
-//   return reverseString(str.slice(1)) + str[0];
-// }
