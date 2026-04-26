adf1fcbba653408b58eaadb1f40c7725e23e9666
mdriduanislam <mohhammadris786@gmail.com>
2026-04-26 06:31:12 +0600

    first 1 of 75 problems

---
+++ leetcode1_75.js
@@ -0,0 +1,13 @@
+
+var mergeAlternately = function(word1, word2) {
+    let mergedString ='';
+    let w1= word1.length;
+    let w2 = word2.length;
+    let max= Math.max(w1,w2)
+    for (let i=0; i<max; i++){
+        mergedString+=word1[i];
+        mergedString+=word2[i];
+    }
+    return mergedString;
+};
+console.log(mergeAlternately("abc","pqr"))//"
